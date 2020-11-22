654cd484d6a7a9eb22a016ecdbdad8e027bd15c0
Fernando Britto <fernandobritto.web@gmail.com>
2020-11-22 13:49:33 -0300

    add Editorconfig

--- index.test.js
+++ index.test.js
@@ -1,1 +1,11 @@
 const Countries = require('./index')
+
+const countries = new Countries()
+
+describe('Countries', () => {
+    describe('class', () => {
+        test('should be an instance of class Countries', () => {
+            expect(countries).toBeInstanceOf(Countries)
+        })
+    })
+})
